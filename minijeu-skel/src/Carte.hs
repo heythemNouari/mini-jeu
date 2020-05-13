1e8a52de8e7a31206e63bf5a5fe350980acf081d
heythemNouari <haitem97real@gmail.com>
2020-05-13 03:15:37 +0200

    show fonctionne

--- minijeu-skel/src/Carte.hs
+++ minijeu-skel/src/Carte.hs
@@ -48,10 +48,21 @@ toList map = M.foldrWithKey (\_ a b -> a:b ) [] map
 
 instance Show Carte where
   show (Carte cartel carteh carte_contenu) =
-    MT.prettyMatrix  (MT.fromList ch cl (toList carte_contenu))
-    -- M.foldWithKey (\_ coord casee accummulateur -> accummulateur ++ (show casee) ) "" carte_contenu 
+   M.foldlWithKey (\ acc  coord casee -> acc ++ (show casee) ) "" carte_contenu
+     
 
+addLines :: Int -> String -> String
+addLines 0 s = if (S.strNull s ) then "" 
+				  else let str = (head s : []) ++ "\n"
+					in str ++ (addLines 6 (tail s)) 
+addLines n s = if (S.strNull s ) then "" 
+				  else (head s : []) ++ (addLines (n - 1) (tail s))
 
+-- on peut la mettre aussi dans le main 
+writeCarteInFile :: Carte -> FilePath-> IO ()
+writeCarteInFile  c@(Carte l h m) fp = do 
+		         	                          let str = addLines (l - 1) ( show c )
+	         	 	                          writeFile fp str 
 -- ######################################################
       -- Read 
 -- ######################################################
@@ -83,8 +94,15 @@ instance Read Carte where
 
 
 initCarte :: Carte
-initCarte = Carte 7 9 initLaby
+initCarte = Carte 5 9 initLaby
 
 initLaby :: M.Map Coord Case
-initLaby = M.fromList [((C 0 0),Mur),((C 0 1),Porte NS Ouverte ),((C 0 2),Entree),((C 0 3),Normal),((C 0 4),Mur),
-		       ((C 1 0),Mur) , ((C 1 1),Mur),((C 1 2),Mur),((C 1 3),Sortie),((C 1 4),Mur)]
+initLaby = M.fromList [((C 0 0),Mur),((C 0 1),Mur ),((C 0 2),Entree),((C 0 3),Normal),((C 0 4),Mur),
+		                    ((C 1 0),Mur) , ((C 1 1),Mur),((C 1 2),Mur),((C 1 3),Sortie),((C 1 4),Mur),
+                        ((C 1 0),Mur) , ((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur),
+                        ((C 1 0),Mur) , ((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur),
+                        ((C 1 0),Mur) , ((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur),
+                        ((C 1 0),Mur) , ((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur),
+                        ((C 1 0),Mur) , ((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur),
+                        ((C 1 0),Mur) , ((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur),
+                        ((C 1 0),Mur) , ((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur) ,((C 1 0),Mur)]
