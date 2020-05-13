132ce489dbe1d424d3ececa582adebdbcb5e468b
heythemNouari <haitem97real@gmail.com>
2020-05-13 19:05:08 +0200

    updt

--- minijeu-skel/src/Environnement.hs
+++ minijeu-skel/src/Environnement.hs
@@ -3,7 +3,7 @@ module Environnement where
 import SDL
 import qualified Data.Map as M
 import Data.List (foldl')
-
+import Carte as Carte
 
 import Entite (Entite)
 import Data.Set (Set)
@@ -13,16 +13,16 @@ data Envi = Envi { contenu_envi :: M.Map Coord [ Entite ]}
 
 
 franchissable_env :: Coord -> Envi -> Bool
-franchissable_env coord (env cnt) = if (contenu M.! coord ) == Monstre x ) then false 
+franchissable_env coord (Envi cnt) = if ((contenu M.! coord ) == Monstre x ) then false 
                                                                     else true
 
 
 -- a refaire 
 
 trouve_id :: Int -> Envi -> Maybe (Coord, Entite)
-trouve_id x env = if ((findWithDefault (Joueur 50) x) == (Joueur 50)) then Nothing
-                                                                      else Just (findWithDefault (Joueur 50) x)
+trouve_id x env = if ((M.findWithDefault (Joueur 50) x) == (Joueur 50)) then Nothing
+                                                                      else Just (M.findWithDefault (Joueur 50) x)
 
-rm_env_id :: Int -> Envi -> Envi
+--rm_env_id :: Int -> Envi -> Envi
 
-bouge_id :: Int -> Coord -> Envi -> Envi
+--bouge_id :: Int -> Coord -> Envi -> Envi
